42da796bcdcd9634f69e998c719be168f551cdab
Nikhil <nikhiljain1124@gmail.com>
2021-04-23 16:12:10 +0530

    Initial commit

---
+++ restaurant.js
@@ -0,0 +1,20 @@
+let hamburger = document.querySelector(".hamburger");
+let menu = document.querySelector(".navbar");
+let bod = document.querySelector(".container");
+
+hamburger.addEventListener("click", function () {
+hamburger.classList.toggle("isactive");
+menu.classList.toggle("active");
+});
+
+
+
+$(window).on('scroll',function(){
+    if($(window).scrollTop()){
+        $('header').addClass('black');
+    }
+    else{
+        $('header').removeClass('black');
+    }
+})
+
