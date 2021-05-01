d2b22e618f0bbc35f227d55b366bf54fc10fe488
nikhiljain7099 <83060346+nikhiljain7099@users.noreply.github.com>
2021-05-01 15:32:44 +0530

    Update restaurant.js

--- restaurant.js
+++ restaurant.js
@@ -1,7 +1,7 @@
 let hamburger = document.querySelector(".hamburger");
 let menu = document.querySelector(".navbar");
 let bod = document.querySelector(".container");
-
+     
 hamburger.addEventListener("click", function () {
 hamburger.classList.toggle("isactive");
 menu.classList.toggle("active");
